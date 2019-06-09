c2773230b65857815403715e4e5e81b70cd5c697
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-06-09 13:03:17 -0300

    Adds root level styles

--- src/mars-explorer/index.jsx
+++ src/mars-explorer/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
+import styled from "styled-components";
 
 import createStore from "mars-explorer/createStore";
 import MarsExplorerRoutes from "mars-explorer/routes";
 
+const RootStyles = styled.div`
+  font-family: sans-serif;
+`;
+
 const MarsExplorer = () => (
-  <Provider store={createStore()}>
-    <MarsExplorerRoutes />
-  </Provider>
+  <RootStyles>
+    <Provider store={createStore()}>
+      <MarsExplorerRoutes />
+    </Provider>
+  </RootStyles>
 );
 
 export default MarsExplorer;
