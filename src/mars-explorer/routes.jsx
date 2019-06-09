0829afaa2e526a2d34a5d613ac2eb25826a56228
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-06-09 13:07:13 -0300

    Adds feed page

--- src/mars-explorer/routes.jsx
+++ src/mars-explorer/routes.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import {HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import RoverProfilePage from "mars-explorer/pages/rover-profile";
 import RoversPage from "mars-explorer/pages/rovers";
 import AboutPage from "mars-explorer/pages/about";
+import FeedPage from "mars-explorer/pages/feed";
 import NotFound from "mars-explorer/pages/not-found";
 
-const MarsExplorerRoutes = () => 
-<Router>
+const MarsExplorerRoutes = () => (
+  <Router>
     <Switch>
-        <Route path="/rovers/:id" component={RoverProfilePage} />
-        <Route path="/rovers" component={RoversPage}/>
-        <Route path="/about" component={AboutPage} />
-        <Route path="/" render={() => <NotFound redirectTo="/rovers" afterSeconds={5} />}/>
+      <Route path="/rovers/:id" component={RoverProfilePage} />
+      <Route path="/rovers" component={RoversPage} />
+      <Route path="/about" component={AboutPage} />
+      <Route exact path="/" component={FeedPage} />
+      <Route
+        path="/"
+        render={() => <NotFound redirectTo="/" afterSeconds={5} />}
+      />
     </Switch>
-</Router>
+  </Router>
+);
 
 export default MarsExplorerRoutes;
