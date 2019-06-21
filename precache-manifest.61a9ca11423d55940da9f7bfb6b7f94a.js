8dd7523326cd9cf8dac56948fe657e76381c7a2f
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-06-21 11:01:06 -0300

    Updates

---
+++ precache-manifest.61a9ca11423d55940da9f7bfb6b7f94a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9687e75b5058cea4dc74362403a5bf",
+    "url": "/mars-explorer/index.html"
+  },
+  {
+    "revision": "cd985d69acce9f638666",
+    "url": "/mars-explorer/static/js/2.fb192a72.chunk.js"
+  },
+  {
+    "revision": "cf738685f51df96354db",
+    "url": "/mars-explorer/static/js/main.53aeaaeb.chunk.js"
+  },
+  {
+    "revision": "ebd2ff42ab58fe57b125",
+    "url": "/mars-explorer/static/js/runtime~main.afde51e1.js"
+  }
+]);
